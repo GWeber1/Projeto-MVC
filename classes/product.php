e440b00a224897a13fdc1bffcf54adc7ad4a5a33
GWeber1 <53956475+GWeber1@users.noreply.github.com>
2021-06-26 11:23:47 -0300

    Adicionada nova tabela de dados

--- classes/product.php
+++ classes/product.php
@@ -7,28 +7,27 @@ class Produtos {
 
   public function MostraProdutos() {
 
-      $connection = new Connection( //Conexão com o banco de dados
+      $connection = new Connection(
           'mysql:host=localhost;dbname=gabrielusina_teste',
           'gabrielusina_gabrielusina',
           'mdD=i0B{q-)c'
       );
       $db = new Database($connection);
 
-      $result_produtos = $db->from('produtos') /*SELECT id_produtos, nome_produtos, data_validade_produtos, nome_fornecedores, id_fornecedores FROM
-                                                produtos; */
+      $result_produtos = $db->from('produtos')
                         ->select(['id_produtos', 'nome_produtos', 'data_validade_produtos', 'nome_fornecedores', 'id_fornecedores'])
                         ->all();
 
      $showProducts = $result_produtos;
-     // Descomente as linhas echo abaixo caso queira realizar um teste na classe
+     // $showProducts = "merda";
     //echo 'Nome do Produto: ' . $this->nome_produtos, '<br>';
     //echo 'Data de Validade: ' . $this->data_validade_produtos, '<br>';
     //echo '<br>';
 
-    return $showProducts; //variável que retorna dados ao controlador
+    return $showProducts;
   }
 
-  public function MostraFornecedores() { //Conexão com o banco de dados
+  public function MostraFornecedores() {
     $connection = new Connection(
         'mysql:host=localhost;dbname=gabrielusina_teste',
         'gabrielusina_gabrielusina',
@@ -36,13 +35,37 @@ class Produtos {
     );
     $db = new Database($connection);
 
-    $result_fornecedores = $db->from('produtos') /*SELECT id_fornecedores, nome_fornecedores FROM produtos */
+    $result_fornecedores = $db->from('produtos')
                                 ->select(['id_fornecedores', 'nome_fornecedores'])
                                 ->all();
 
 
-    $showFornecedores = $result_fornecedores; 
+    $showFornecedores = $result_fornecedores;
 
-    return $showFornecedores; //variável que retorna dados ao controlador
+    return $showFornecedores;
+  }
+
+  public function MostraRegioes() {
+    $connection = new Connection(
+      'mysql:host=localhost;dbname=gabrielusina_teste',
+      'gabrielusina_gabrielusina',
+      'mdD=i0B{q-)c'
+    );
+    $db = new Database($connection);
+
+    $result_regioes = $db->from('cidade_estado')
+    ->select(['id_cidade_estado', 'estado', 'cidade'])
+    ->all();
+
+    $showRegioes = $result_regioes;
+
+    return $showRegioes;
   }
 }
+
+//  public function MostraFornecedores() {
+//    $showFornecedores = $this->id_fornecedores . $this->nome_fornecedores;
+
+    //echo 'Nome do Fornecedor: . $this->nome_fornecedores, '<br>';
+
+  //  return $showFornecedores;
